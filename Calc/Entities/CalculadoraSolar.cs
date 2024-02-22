47184c6469e62176f64182ba647c2e7950b980f8
Rômulo <Rmulo@ROMULOSOLAR>
2024-02-22 16:16:52 -0300

    Adicionar arquivos de projeto.

---
+++ Calc/Entities/CalculadoraSolar.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Calc.Entities;
+
+internal class CalculadoraSolar
+{
+    public double Consumo;
+    public double Potencia;
+
+    public double PercaSistema = 0.84;
+    public double IrradiacaoTo = 4.9;
+    public double PotenciaModulo = 575;
+    public int Dias = 30;
+
+    public double CalculaPotencia()
+    {
+        return Consumo * 1000 / Dias / IrradiacaoTo / PercaSistema /1000;
+    }
+
+    public override string ToString()
+    {
+        return $"A potencia do sistema é : " + CalculaPotencia().ToString("F2",CultureInfo.InvariantCulture) +$"kWp";
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Calc.Entities;
+using System.Globalization;
+
+CalculadoraSolar calc = new CalculadoraSolar();
+
+Console.WriteLine("Calcular a Quantidade de Módulos fotovoltaicos");
+Console.WriteLine();
+Console.Write("Informe Seu Consumo de Energia em Kwh: ");
+calc.Consumo = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(calc);
+Console.ReadLine();
