ae5d72a841320ecf7892502080ab9dd06da78b87
Romulo Santos Ribeiro <romulosnik@hotmail.com>
2024-03-07 13:51:05 -0300

    implementado a calculo da quantidade de modulos.

--- Calc/Entities/CalculadoraSolar.cs
+++ Calc/Entities/CalculadoraSolar.cs
@@ -14,11 +14,17 @@ internal class CalculadoraSolar
 
     public double CalculaPotencia()
     {
-        return Consumo * 1000 / Dias / IrradiacaoTo / PercaSistema /1000;
+        return Consumo * 1000 / Dias / IrradiacaoTo / PercaSistema / 1000;
+    }
+    public double CalcularQuantidadeModulos()
+    {
+        double Calculo = CalculaPotencia() * 1000 / PotenciaModulo;
+
+        return Math.Ceiling(Calculo);
     }
 
     public override string ToString()
     {
-        return $"A potencia do sistema é : " + CalculaPotencia().ToString("F2",CultureInfo.InvariantCulture) +$"kWp";
+        return $"A potencia do sistema é : " + CalculaPotencia().ToString("F2",CultureInfo.InvariantCulture) +$" kWp - Total de " + CalcularQuantidadeModulos() + $" Modulo(s) de " + PotenciaModulo +" Wp";
     }
 }
